17a6fec174bc8fdb42e3f27d692f63c10cb30ed6
pablo <pablohgm@gmail.com>
2016-07-09 11:20:46 -0600

    migration complete npm dependencies are updated

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
 'use strict';
-const app = require('app');
-const BrowserWindow = require('browser-window');
+const electron = require('electron')
+
+// Module to control application life.
+const app = electron.app
+
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
 
 // Initiate backend `LoopBack`
 require(__dirname+'/server/server.js');
 
+//toFIX
 // report crashes to the Electron project
-require('crash-reporter').start();
+//require('crash-reporter').start();
 
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
 
-// prevent window being garbage collected
-let mainWindow;
 
-function onClosed() {
-    // dereference the window
-    // for multiple windows store them in an array
-    mainWindow = null;
+let mainWindow
+
+function createWindow () {
+	mainWindow = new BrowserWindow({width: 800, height: 600})
+	mainWindow.loadURL('http://localhost:3000/index.html')
+
+	// Open the DevTools.
+	//mainWindow.webContents.openDevTools()
+
+	// Emitted when the window is closed.
+	mainWindow.on('closed', function () {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null
+	})
 }
 
-function createMainWindow() {
-    const win = new BrowserWindow({
-        width: 600,
-        height: 400
-    });
+app.on('ready', createWindow)
 
-    win.loadUrl('http://localhost:3000/index.html');
-    win.maximize();
-    win.on('closed', onClosed);
+app.on('window-all-closed', function () {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
 
-    return win;
-}
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate-with-no-open-windows', () => {
-    if (!mainWindow) {
-        mainWindow = createMainWindow();
-    }
-});
-
-app.on('ready', () => {
-    mainWindow = createMainWindow();
-});
+app.on('activate', function () {
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow()
+	}
+})
