a12ea82a2214042b89757c0e1eb2464d44a31549
pablohgm <paglohgm@gmail.com>
2017-07-02 15:18:49 -0600

    unstable report

--- common/models/contribuyente.js
+++ common/models/contribuyente.js
@@ -1,73 +1,85 @@
 module.exports = function(Contribuyente) {
-
-		var jsreport = require('jsreport');
-		var fs = require('fs');
-		var app = require('../../server/server');
+	var jsreport = require('jsreport');
+	var fs = require('fs');
+	var app = require('../../server/server');
+	var exec = require('child_process');
 
     Contribuyente.report = function(argData, cb){
-				var configModel = app.models.Configuration;
+		var configModel = app.models.Configuration;
 
-				configModel.find({limit: 1}, function(err, models){
-						if(models){
-								var tmpConfig = models[0];
-								argData.mensaje = tmpConfig.mensaje;
-						}
-						jsreport.render({
-							template: {
-								content: fs.readFileSync('./client/views/reciboTemplate.html',  'utf8'),
-								engine: 'jsrender',
-								recipe: 'phantom-pdf',
-								phantom: {
-									phantomjsVersion: "2.1.1",
-									margin: { top: 0, right: 0, bottom: 0, left: 0 }
-								}
-							},
-							data: formatDataTemplate(argData)
-						}).then(function(out) {
-							out.stream.pipe(fs.createWriteStream('./report.pdf'));
-							cb(null, 'La aplicacion ha creado el reporte, abriendo ...');
-						}).catch(function(e) {
-							cb(e.message);
-						});
-				});
+		configModel.find({limit: 1}, function(err, models){
+			if(models){
+					var tmpConfig = models[0];
+					argData.mensaje = tmpConfig.mensaje;
+			}
+			jsreport.render({
+				template: {
+					content: fs.readFileSync('./client/views/reciboTemplate.html',  'utf8'),
+					engine: 'jsrender',
+					recipe: 'phantom-pdf',
+					phantom: {
+						format: 'A4',
+						margin: { top: 0, right: 0, bottom: 0, left: 0 }
+					}
+				},
+				data: formatDataTemplate(argData)
+			}).then(function(out) {
+				out.stream.pipe(fs.createWriteStream('./report.pdf'))
+					.on('finish', openReport);
+				cb(null, 'La aplicacion ha creado el reporte, abriendo ...');
+			}).catch(function(e) {
+				cb(e.message);
+			});
+		});
     };
 
-		function formatDataTemplate (argData) {
-				var tmpTriplets = {},
-						tmpCont = 0;
-				tmpTriplets.pages = [];
+	function openReport() {
+		console.log('open report ...');
+		exec.exec('start "" /max "./report.pdf"', puts);
+	};
 
-				for(var i = 0; i < argData.contribuyentes.length; i++){
-						tmpTriplets.pages.push({
-								contribuyentes: []
-						});
-						tmpCont = 0;
-						while (tmpCont < 3 && argData.contribuyentes.length){
-								var tmpContribuyente = argData.contribuyentes.pop();
-								tmpTriplets.pages[i].contribuyentes.push({
-											year: argData.year,
-											month: argData.month,
-											day: argData.day,
-											name: tmpContribuyente.nombre,
-											lastname: tmpContribuyente.Apellido1,
-											lastname2: tmpContribuyente.Apellido2,
-											sector: (tmpContribuyente.Sector === undefined) ? '' : tmpContribuyente.Sector.informacion,
-											address: tmpContribuyente.direccion,
-											value: tmpContribuyente.monto,
-											message: argData.mensaje
-								});
-								tmpCont++;
-						}
-				}
-				return tmpTriplets;
-		};
+	function puts(error, stdout, stderr) {
+		console.log(stdout);
+		console.log(error);
+		console.log(stderr);
+	};
 
-		Contribuyente.remoteMethod('report', {
-			accepts: { arg: 'argData', type: 'object', http: { source: 'body' } },
-			returns: [
-				{ arg: 'msg', type: 'string'},
-				{ arg: 'Content-Type', type: 'string', http: { target: 'header' } },
-			]
-		});
+	function formatDataTemplate (argData) {
+		var tmpTriplets = {},
+				tmpCont = 0;
+		tmpTriplets.pages = [];
+
+		for(var i = 0; i < argData.contribuyentes.length; i++){
+			tmpTriplets.pages.push({
+					contribuyentes: []
+			});
+			tmpCont = 0;
+			while (tmpCont < 3 && argData.contribuyentes.length){
+				var tmpContribuyente = argData.contribuyentes.pop();
+				tmpTriplets.pages[i].contribuyentes.push({
+						year: argData.year,
+						month: argData.month,
+						day: argData.day,
+						name: tmpContribuyente.nombre,
+						lastname: tmpContribuyente.Apellido1,
+						lastname2: tmpContribuyente.Apellido2,
+						sector: (tmpContribuyente.Sector === undefined) ? '' : tmpContribuyente.Sector.informacion,
+						address: tmpContribuyente.direccion,
+						value: tmpContribuyente.monto,
+						message: argData.mensaje
+				});
+				tmpCont++;
+			}
+		}
+		return tmpTriplets;
+	};
+
+	Contribuyente.remoteMethod('report', {
+		accepts: { arg: 'argData', type: 'object', http: { source: 'body' } },
+		returns: [
+			{ arg: 'msg', type: 'string'},
+			{ arg: 'Content-Type', type: 'string', http: { target: 'header' } },
+		]
+	});
 
 };
